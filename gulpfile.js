3b0f6b19c691405ce1f367a75cb9d9c50408a5a2
Jason <christoff@gmail.com>
2015-08-07 15:37:52 +0800

    build

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,19 @@
 var gulp = require('gulp');
 var server = require('karma').Server;
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var annotate = require('gulp-ng-annotate');
 
-gulp.task('build', function (done) {
-
+gulp.task('build', function () {
+	return gulp.src('src/**/*.js')
+		.pipe(annotate())
+		.pipe(uglify())
+		.pipe(rename(function (path) {
+			path.dirname = '/';
+			path.extname = '.min.js';
+		}))
+		.pipe(gulp.dest('dist/'));
 });
 
 gulp.task('test', function (done) {
